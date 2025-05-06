8251f2bfa9217368fab05893cb5bdf38f3519411
da1nda2n <da1nda2n@gmail.com>
2025-05-07 01:39:56 +0900

    Initial commit

---
+++ src/main/java/com/example/uniton/UnitonApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniton/UnitonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
