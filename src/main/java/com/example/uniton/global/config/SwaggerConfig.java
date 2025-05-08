a779d1f951399acfcb66aa3fbbd33779c2c5d4a9
da1nda2n <da1nda2n@gmail.com>
2025-05-08 16:45:40 +0900

    [Env] Config 및 jwt 설정

---
+++ src/main/java/com/example/uniton/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.uniton.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.url(serverUrl);
+
+        return new OpenAPI()
+                .addServersItem(server)
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Spring Boot Test")
+                .description("Specification")
+                .version("1.0.0");
+    }
+}
